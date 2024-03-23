c4a770b83b14223f70f4e52a474dad49fba97107
icep0ps <tapsmuko@gmail.com>
2024-03-24 00:15:21 +0200

    added basic relationships

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,10 +2,23 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Product extends Model
 {
     use HasFactory;
+
+    public function reviews(): HasMany
+    {
+        return $this->hasMany(ProductReview::class);
+    }
+    public function category(): BelongsTo
+    {
+        return $this->BelongsTo(ProductCategory::class);
+    }
+    
+    
 }
