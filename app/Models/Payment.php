cf389fe690677374544c87805231ff81c03199de
Takudzwa Michael Tsuro <42871110+michaeltsuro@users.noreply.github.com>
2024-03-29 22:43:22 +0200

    Payment relationship with other models

--- app/Models/Payment.php
+++ app/Models/Payment.php
@@ -4,13 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Payment extends Model
 {
     use HasFactory;
 
-    public function orders()
+    public function order(): BelongsTo
     {
-        return $this->hasMany(Order::class);
+        return $this->belongsTo(Order::class);
     }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+    
 }
