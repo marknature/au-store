640042ecaf629455580560e87b54b152dd381116
Nature <marknature_@outlook.com>
2024-03-31 14:23:10 +0200

    fixed error

--- app/Models/Payment.php
+++ app/Models/Payment.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Payment extends Model
 {
